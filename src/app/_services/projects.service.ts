1f48cce9f984c0194d15de86a6bed2a801714f58
Alexey Gaulke <alexeygaulke@Alexeys-MacBook-Air.local>
2025-01-19 16:44:22 -0700

    fixed typo

--- src/app/_services/projects.service.ts
+++ src/app/_services/projects.service.ts
@@ -7,7 +7,7 @@ import { Tag } from '../_models/tag';
 })
 export class ProjectsService {
   projects: Project[] = [
-    {id: 0, name: "Website Portfolio", pictures: ["../../assets/alexey.png","../../assets/Image2.png","../../assets/Image3.png"], projectLink: "//github.com/Agaulke2025/Portfolio.git", summary: "Project that includes information like projects, my resume, and contact information", description: "I tackled this project in order to prove my understanding of frontend development specifically with Angular. This project greatly improved my skills with typescript and overarching concepts within the Angular and Bootstrap frameworks", tags: [Tag.ANGULAR, Tag.TYPESCRIPT, Tag.BOOTSTRAP, Tag.HTML, Tag.CSS]},
+    {id: 0, name: "Website Portfolio", pictures: ["../../assets/Portfolio1.png","../../assets/Portfolio2.png"], projectLink: "//github.com/Agaulke2025/Portfolio.git", summary: "Project that includes information like projects, my resume, and contact information", description: "I tackled this project in order to prove my understanding of frontend development specifically with Angular. This project greatly improved my skills with typescript and overarching concepts within the Angular and Bootstrap frameworks", tags: [Tag.ANGULAR, Tag.TYPESCRIPT, Tag.BOOTSTRAP, Tag.HTML, Tag.CSS]},
     {id: 1, name: "Database System Project", pictures: [], projectLink: "", summary: "MySQL database project for a logistics company", description: "This project was part of an Intro to Databases course in which we developed a functioning MySQL database with a Java interface", tags: [Tag.MYSQL, Tag.JAVA]},
   ];
   constructor() { }
